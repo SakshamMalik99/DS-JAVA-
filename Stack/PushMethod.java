42899f287d3e22713907e0b93a32e69e199d4e36
Saksham Malik <54699504+SaKsHaMaLiK@users.noreply.github.com>
2020-08-07 21:57:19 +0530

    Add files via upload

---
+++ Stack/PushMethod.java
@@ -0,0 +1,11 @@
+import java.util.Stack;
+
+class PushMethod {
+    public static void main(String[] args) {
+        Stack<String> animals = new Stack<>();
+        animals.push("Dog");
+        animals.push("Horse");
+        animals.push("Cat");
+        System.out.println("Stack: " + animals);
+    }
+}
