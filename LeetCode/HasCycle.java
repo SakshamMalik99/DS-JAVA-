c5d3673fc72f8b13871e2d5a08e6d3e10593465c
Saksham Malik <54699504+SaKsHaMaLiK@users.noreply.github.com>
2020-08-08 22:14:28 +0530

    Add files via upload

---
+++ LeetCode/HasCycle.java
@@ -0,0 +1,12 @@
+public class HasCycle {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast)
+                return true;
+        }
+        return false;
+    }
+}
