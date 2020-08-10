3df081aab8f8b0584f64129be37a855b0a8a8925
Saksham Malik <54699504+SaKsHaMaLiK@users.noreply.github.com>
2020-08-10 13:07:07 +0530

    Add files via upload

---
+++ LeetCode/DeleteNode.java
@@ -0,0 +1,8 @@
+class DeleteNode {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        ListNode temp = node.next;
+        node.next = node.next.next;
+        temp.next = null;
+    }
+}
