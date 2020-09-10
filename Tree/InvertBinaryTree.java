3d4fefdd668757504ff324f679e2156b423de0a0
Saksham Malik <54699504+SaKsHaMaLiK@users.noreply.github.com>
2020-09-10 09:42:47 +0530

    Add files via upload

---
+++ Tree/InvertBinaryTree.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+public class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+
+        TreeNode temp = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(temp);
+
+        return root;
+    }
+}
